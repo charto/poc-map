eda30a23a974c8aac379f03f63ac20e61f5f90bb
jjrv <juha@busfaster.com>
2015-11-17 18:12:01 +0200

    Add bundling script for production use.

--- config.js
+++ config.js
@@ -7,8 +7,16 @@ System.config({
     "github:*": "jspm_packages/github/*"
   },
 
+  meta: {
+    "clean-css": {
+      "loader": "charto/plugin-node"
+    }
+  },
+
   map: {
     "bluebird": "npm/bluebird/js/browser/bluebird.min.js",
+    "charto/plugin-node": "github:charto/plugin-node@master",
+    "clean-css": "npm/clean-css/index.js",
     "css": "github:systemjs/plugin-css@0.1.19",
     "leaflet": "npm/leaflet/dist/leaflet.js",
     "leaflet.css": "npm/leaflet/dist/leaflet.css"
